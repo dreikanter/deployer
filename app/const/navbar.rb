3fd5e34972230d8ac20d4773b0a7278de7437b6a
Alex Musayev <alex.musayev@gmail.com>
2016-08-10 20:55:55 +0300

    Test menu item removed

--- app/const/navbar.rb
+++ app/const/navbar.rb
@@ -5,11 +5,6 @@ module Const
         caption: 'Deployment keys',
         path: Rails.application.routes.url_helpers.deployment_keys_path,
         controller: 'deployment_keys'
-      },
-      {
-        caption: 'Sample item',
-        path: '#',
-        controller: ''
       }
     ].freeze
   end
