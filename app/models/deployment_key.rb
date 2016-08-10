f87fb56c83f95ecefe9d8e3048fa7c6a62cd9540
Alex Musayev <alex.musayev@gmail.com>
2016-08-10 17:18:44 +0300

    Annotations

--- app/models/deployment_key.rb
+++ app/models/deployment_key.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: deployment_keys
+#
+#  id                :integer          not null, primary key
+#  encrypted_private :string           not null
+#  public            :string           not null
+#  iv                :string           not null
+#  name              :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class DeploymentKey < ApplicationRecord
 end
--- test/models/deployment_key_test.rb
+++ test/models/deployment_key_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: deployment_keys
+#
+#  id                :integer          not null, primary key
+#  encrypted_private :string           not null
+#  public            :string           not null
+#  iv                :string           not null
+#  name              :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 require "test_helper"
 
 class DeploymentKeyTest < ActiveSupport::TestCase
